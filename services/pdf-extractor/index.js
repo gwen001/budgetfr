547bbd98aa4b533e45498c2f7d97e040f63aec4b
Gwen <g@glc.st>
2026-08-30 17:48:20 +0200

    pdf service

--- services/pdf-extractor/index.js
+++ services/pdf-extractor/index.js
@@ -29,4 +29,5 @@ app.post("/extract", async (req, res) => {
     }
 });
 
-app.listen(4000, () => console.log("Extractor running on 4000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Extractor running on ${PORT}`));
